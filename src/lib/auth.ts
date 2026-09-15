7881133447219ec7606aa3f5bbdd3fdc313492cf
alimmondal <italimbd@gmail.com>
2026-09-15 17:56:57 +0600

    upd

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -27,5 +27,13 @@ export const auth = betterAuth({
     },
     emailAndPassword: { 
     enabled: true, 
+    autoSignIn: false,
+    requireEmailVerification: true
   }, 
+  emailVerification: {
+    sendVerificationEmail: async ( { user, url, token }, request) => {
+      console.log("Email verification");
+      
+    }
+  }
 });
