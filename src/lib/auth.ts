d514650ee3d671083482169f6185390a3ef41aee
alimmondal <italimbd@gmail.com>
2026-09-19 14:27:50 +0600

    updatd

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -13,13 +13,13 @@ const transporter = nodemailer.createTransport({
   },
 });
 
-transporter.verify((error, success) => {
-  if (error) {
-    console.error("SMTP connection failed:", error);
-  } else {
-    console.log("SMTP server is ready:", success);
-  }
-});
+// transporter.verify((error, success) => {
+//   if (error) {
+//     console.error("SMTP connection failed:", error);
+//   } else {
+//     console.log("SMTP server is ready:", success);
+//   }
+// });
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
@@ -51,6 +51,7 @@ export const auth = betterAuth({
   },
   emailVerification: {
     sendOnSignUp: true,
+    autoSignInAfterVerification: true,
     sendVerificationEmail: async ({ user, url, token }, request) => {
       try {
         const verificationUrl = `${process.env.APP_URL}/verify-email?token=${token}`;
@@ -225,7 +226,7 @@ ${verificationUrl}`,
                         target="_blank"
                         style="color: #2563eb;"
                       >
-                        ${verificationUrl}
+                        ${url}
                       </a>
                     </p>
 
@@ -289,6 +290,14 @@ ${verificationUrl}`,
         console.log(error);
         throw error;
       }
+    },
+  },
+  socialProviders: {
+    google: {
+      accessType: "offline",
+      prompt: "select_account consent",
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     },
   },
 });
--- src/modules/post/post.controller.ts
+++ src/modules/post/post.controller.ts
@@ -3,7 +3,15 @@ import { PostService } from "./post.service";
 
 const createPost = async (req: Request, res: Response) => {
   try {
-    const result = await PostService.createPost(req.body);
+    const user = req.user;
+    if(!user){
+      return res.status(400).json({
+        error:"Post creation error",
+    })
+    }
+    
+    const result = await PostService.createPost(req.body, user.id as string);
+    
     res.status(202).json(result);
   } catch (error) {
     res.status(400).json({
