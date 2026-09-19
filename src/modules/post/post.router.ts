8dcc05ca78958d1051f900156944b358849e223b
alimmondal <italimbd@gmail.com>
2026-09-19 14:46:00 +0600

    updated user session

--- src/modules/post/post.router.ts
+++ src/modules/post/post.router.ts
@@ -1,69 +1,68 @@
 import express, { Request, Response, NextFunction } from "express";
 import { postController } from "./post.controller";
 // import { betterAuth } from "better-auth";
-import { auth as betterAuth } from "../../lib/auth";
-import { success } from "better-auth";
+import auth, { UserRole } from "../../middlewares/auth";
 const router = express.Router();
 
-export enum UserRole {
-    USER ="USER",
-    ADMIN = "ADMIN"
+// export enum UserRole {
+//     USER ="USER",
+//     ADMIN = "ADMIN"
 
-}
+// }
 
-declare global {
-    namespace Express {
-        interface Request{
-            user?:{
-                id: string;
-                email: string;
-                name: string;
-                role: string;
-                emailVerified: boolean;
-            }
-        }
-    }
-}
+// declare global {
+//     namespace Express {
+//         interface Request{
+//             user?:{
+//                 id: string;
+//                 email: string;
+//                 name: string;
+//                 role: string;
+//                 emailVerified: boolean;
+//             }
+//         }
+//     }
+// }
 
-const auth = (...roles: UserRole[]) => {
-  return async (req: Request, res: Response, next: NextFunction) => {
-    // get session
-    const session = await betterAuth.api.getSession({
-      headers: req.headers as any,
-    });
+// const auth = (...roles: UserRole[]) => {
+//   return async (req: Request, res: Response, next: NextFunction) => {
+//     // get session
+//     const session = await betterAuth.api.getSession({
+//       headers: req.headers as any,
+//     });
 
-    if (!session) {
-      return res.status(401).json({
-        success: false,
-        message: "your not authorized",
-      });
-    }
-    if (!session.user.emailVerified) {
-      return res.status(403).json({
-        success: false,
-        message: "Your email is not verified yet, please, verify your email",
-      });
-    }
+//     if (!session) {
+//       return res.status(401).json({
+//         success: false,
+//         message: "your not authorized",
+//       });
+//     }
+//     if (!session.user.emailVerified) {
+//       return res.status(403).json({
+//         success: false,
+//         message: "Your email is not verified yet, please, verify your email",
+//       });
+//     }
 
-    req.user={
-        id: session.user.id,
-        email: session.user.email,
-        name: session.user.name,
-        role: session.user.role as string,
-        emailVerified: session.user.emailVerified
-    }
+//     req.user={
+//         id: session.user.id,
+//         email: session.user.email,
+//         name: session.user.name,
+//         role: session.user.role as string,
+//         emailVerified: session.user.emailVerified
+//     }
 
-    if(!roles.length && roles.includes(req.user.role as UserRole) ){
-        return res.status(403).json({
-        success: false,
-        message: "Forbidden! You don't have access to this resources.",
-      });
-    }
+//     if(!roles.length && roles.includes(req.user.role as UserRole) ){
+//         return res.status(403).json({
+//         success: false,
+//         message: "Forbidden! You don't have access to this resources.",
+//       });
+//     }
 
-    // console.log(session);
-    next();
-  };
-};
+//     // console.log(session);
+//     next();
+//   };
+// };
 
 router.post("/", auth(UserRole.USER), postController.createPost);
 
