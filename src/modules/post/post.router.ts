6260de7f7dc3d3e0597ac536dd4432b813ca6742
alimmondal <italimbd@gmail.com>
2026-09-20 08:47:04 +0600

    upd

--- src/modules/post/post.router.ts
+++ src/modules/post/post.router.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response, NextFunction } from "express";
+import express from "express";
 import { postController } from "./post.controller";
 // import { betterAuth } from "better-auth";
 import auth, { UserRole } from "../../middlewares/auth";
